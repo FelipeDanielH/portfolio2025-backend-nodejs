cc1fd582b00372c3a6adf83b5bc481ab9fa1d059
Felipe Daniel <felipe.daniel.henriquez@gmail.com>
2025-07-13 13:12:54 -0400

    feat: agrega documentacion con swagger

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import { connectDB } from './config/db';
 import homeRoutes from './routes/Home.routes';
 import { notFoundHandler } from './middlewares/notFoundHandler';
 import { errorHandler } from './middlewares/errorHandler';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
 
 dotenv.config();
 connectDB();
@@ -13,14 +15,21 @@ const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
 
+// 🚨 Swagger debe ir antes de notFoundHandler
+const swaggerDocument = YAML.load('./src/docs/swagger.yaml');
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// Rutas API
 app.use('/home', homeRoutes);
 
-app.use(notFoundHandler);
-app.use(errorHandler);
-
+// Ruta raíz
 app.get('/', (_req, res) => {
   res.send('API del Portafolio Backend funcionando 🧠');
 });
+
+// Middlewares de error
+app.use(notFoundHandler);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`🚀 Servidor escuchando en http://localhost:${PORT}`);
