7d4999ccc91deabdb301c79be4a28b718abd9baa
Felipe Daniel <felipe.daniel.henriquez@gmail.com>
2025-07-19 16:58:44 -0400

    fix(build): elimina casos de uso home obsoletos que causaban error de importación

--- src/application/use-cases/home/createHome.ts
+++
@@ -1,6 +0,0 @@
-import { HomeRepository } from '../../../domain/interfaces/HomeRepository.interface';
-import { HomeProps } from '../../../domain/entities/Home.entity';
-
-export const createHome = async (repo: HomeRepository, data: HomeProps) => {
-  return await repo.create(data);
-}; 
--- src/application/use-cases/home/deleteHome.ts
+++
@@ -1,5 +0,0 @@
-import { HomeRepository } from '../../../domain/interfaces/HomeRepository.interface';
-
-export const deleteHome = async (repo: HomeRepository) => {
-  return await repo.delete();
-}; 
